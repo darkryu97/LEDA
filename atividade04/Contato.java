fa988a4de5a03afcbba10c77ce98fad68a0439d1
duduzin <edu.obgemail@gmail.com>
2023-11-15 23:35:20 -0300

    FEAT Problematica adicionada

---
+++ atividade04/Contato.java
@@ -0,0 +1,11 @@
+package atividade04;
+
+public class Contato {
+    String nome;
+    int numero;
+
+    public Contato(String nome, int numero) {
+        this.nome = nome;
+        this.numero = numero;
+    }
+}
